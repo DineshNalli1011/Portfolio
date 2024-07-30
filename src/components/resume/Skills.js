9a5cb4ccf1f6a3dc2197d441faeea31592b0119f
Dinesh Das Nalli <123890060+DineshNalli1011@users.noreply.github.com>
2024-07-30 08:53:39 +0530

    Update Skills.js

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -114,7 +114,7 @@ const skillsData = [
       },
       {
         name: "Figma",
-        icon: "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+        icon: "https://w7.pngwing.com/pngs/911/515/png-transparent-figma-logo-brand-logos-brands-in-colors-icon-thumbnail.png",
       },
     ],
   },
